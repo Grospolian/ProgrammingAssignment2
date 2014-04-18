be75c564cea363dad2485ffdf8a71ff457640c0f
SebastianD <sebastian.deutsch@hsrw.org>
2014-04-18 14:16:44 +0200

    Works, more comments needed, Included a det() test

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of 
 
 ## Write a short comment describing this function
 
+## Simple Matrix for Testing
+A <- matrix(c(1,2,0,2,3,0,3,4,1), 3, 3
+
 makeCacheMatrix <- function(x = matrix()) {
+  ##Test if the matrix x is invertable
+  if (det(x) == 0) {
+    print("There is no Inverse for the given matrix!") ##Throw a Warning if det(x) = 0
+  }
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(Inverse) m <<- Inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -12,4 +29,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
